d9725d0fdee91042c28ff27e35f689b72b6bcd59
halima-web <halimamechelouf@gmail.com>
2025-11-26 09:57:47 +0100

    ma première version de mon projet wagtail

---
+++ blog/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+
+# Add these:
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import MultiFieldPanel
+from wagtail.snippets.models import register_snippet
+
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+    # add the get_context method:
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+    # ...
+    #content_panels = Page.content_panels + ["intro"]
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    # Add this:
+    authors = ParentalManyToManyField('blog.Author', blank=True)
+    # Add the main_image method:
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(["date", "authors"], heading="Blog information"),
+        "intro", "body", "gallery_images"
+    ]
+    
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = ["image", "caption"]
+
+    # ... Keep BlogIndexPage, BlogPage, BlogPageGalleryImage models, and then add the Author model:
+@register_snippet
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    author_image = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = ["name", "author_image"]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Authors'
---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-nh%hrl@71t^n1yqrg&xd&&v7+oxre^4f8zy2n35a^9(%u+#$j*"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
